871d10674821e1b8a087c2bd90b05538cf0bd6dc
Danish Sayyed <35421598+thesayyeddanish@users.noreply.github.com>
2026-09-24 21:42:33 +0530

    Update testimonials-data.js

--- assets/js/testimonials-data.js
+++ assets/js/testimonials-data.js
@@ -5,48 +5,55 @@ const TESTIMONIALS = [
     role: 'Head of Performance Analysis, Intelligent Cricket',
     full: 'Danish was a great part of the team at Intelligent Cricket. His ability to visualise data and make it easy to interact with enabled insights to be unlocked by all end users. He showed strong initiative on many tasks and took them on and went above and beyond. I will be watching keenly on his future progress and how he continues to develop.',
     img: './assets/images/bertie.jpg',
-    tags: ["Data Visualization", "Cricket Analytics", "Team Player"]
+    linkedin: "https://www.linkedin.com/in/bertie-kennedy-633101177"
+    tags: ["Data visualization", "Insights", "Stakeholder management"]
   },
   {
     name: 'Waseef Khan',
     role: 'Head Analyst, Lahore Qalandars',
     full: 'Danish played a crucial role in helping me during the initial stages of my career. His guidance, support, and encouragement gave me the confidence to move forward and grow professionally. I am truly grateful for his mentorship and generosity.',
     img: './assets/images/waseef.png',
-    tags: ["Mentorship", "Data Analysis", "Professional Growth"]
+    linkedin: "https://www.linkedin.com/in/waseef-khalid-khan-366951237"
+    tags: ["Mentorship", "Cricket analytics", "Professional growth"]
   },
   {
     name: 'Gaurav Rao',
     role: 'Match Coding Manager, NV Play',
     full: 'I had the pleasure of working closely with Danish during his time at Intelligent Cricket as a Cricket Data Analyst. He consistently impressed me with his data visualization skills and was extremely dedicated to his work. He approached every task with a positive attitude and a strong sense of professionalism. His sincerity, dedication, and exceptional work ethic make him an invaluable addition to any team or organization. I am confident that he will continue to excel in his future endeavors.',
     img: './assets/images/gaurav.jpg',
-    tags: ["Data Visualization", "Attention to Detail", "Collaboration"]
+    linkedin: "https://www.linkedin.com/in/jgauravrao"
+    tags: ["Data visualization", "Professionalism", "Work ethics"]
   },
   {
     name: 'Vaibhav Gupta',
     role: 'Senior Data Analyst, Concentrix',
     full: 'I highly recommend Danish for any data analysis role that requires expertise in Tableau and Power BI. He possesses a unique blend of technical skills, data visualization proficiency, and analytical thinking that make him a valuable asset to any organization. He has demonstrated a deep understanding of both Tableau and Power BI tools and is proficient in creating complex data models, designing interactive dashboards, and implementing advanced features within these platforms.',
     img: './assets/images/vaibhav.png',
-    tags: ["Analytics", "Reliability", "Collaboration"]
+    linkedin: "https://www.linkedin.com/in/vaibhav-gupta-tcet"
+    tags: ["Data expertise", "Analytical thinking", "Dashboards"]
   },
   {
     name: 'Vijay Kumar',
     role: 'Data Analyst, EXL',
     full: 'Danish Sayyed was an exceptional data visualization analyst at Intelligent Cricket and used his expertise to deliver valuable insights that helped teams and coaches make data-driven decisions. His ability to transform complex data into clear, actionable visualizations has been a game-changer for our team. Danish consistently provided high-quality analyses, from player performance to match strategies, helping us gain a competitive edge. I highly recommend him for anyone looking to leverage data for success in cricket analysis.',
     img: './assets/images/vijay.png',
-    tags: ["Data Analysis", "Problem Solving", "Teamwork"]
+    linkedin: "https://www.linkedin.com/in/vijay-kumarj"
+    tags: ["Data Analysis", "Data-driven decisions", "Teamwork"]
   },
   {
     name: 'Chinmay Sahu',
     role: 'Product Ops Analyst, Target',
     full: 'I enthusiastically recommend Danish Sayyed to anyone who is looking for a guidance in data analysis field. He explained his challenges & learning of his transition to this field. Also, he mentioned few key pointers for fellow aspiring data analyst. What sets him apart is his ability to translate complex technical concepts into clear, actionable insights as seen in his multiple posts on LinkedIn. He not only poses knowledge about data analysis but also has proficiency in creating professional designs for dashboards with Figma & Canva.',
     img: './assets/images/chinmay.jpg',
-    tags: ["Product Analytics", "Communication", "Initiative"]
+      linkedin: "https://www.linkedin.com/in/sahuchinmay"
+    tags: ["Guidance", "Communication", "Actionable insights"]
   },
   {
     name: 'Burhanuddin Bhavnagarwala',
     role: 'Data Analyst, Nottingham University',
     full: 'Danish is very talented and creative. A true professional in all he undertakes, he makes sure that the project gets done well and finishes on time. He will go the extra mile to make sure that his department is well recognized and he easily shares credit throughout. It was a real pleasure studying with Danish.',
     img: './assets/images/burhan.jpg',
-    tags: ["Cricket Analytics", "Dedication", "Insight"]
+    linkedin: "https://www.linkedin.com/in/burhanuddin-bhavnagarwala"
+    tags: ["Creativity", "Extra mile", "Team player"]
   },
 ];
